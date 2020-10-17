41d3863492be8e4d0cb5172e9685e1d04d909a56
shridhar <shridhar@qs.com>
2020-10-17 23:25:44 +0530

    Google auth completion

--- routes/auth.js
+++ routes/auth.js
@@ -13,4 +13,12 @@ router.get('/google/callback', passport.authenticate('google', {failureRedirect 
         res.redirect('/dashboard')
     }
     )
+
+
+// @desc Logout user
+// @route GET /auth/logout
+router.get('/logout',(req,res) => {
+    req.logout(),
+    res.redirect('/')
+})
 module.exports = router
